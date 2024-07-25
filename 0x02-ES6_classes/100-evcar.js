3fb2a40ea718b63289bfe9837fee1ea1b7063d67
sibusiso-maope <Sibusiso.Maope@gmail.com>
2024-07-25 12:12:54 -0700

    My Project 30-40% done

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,14 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    const Clone = super.constructor[Symbol.species];
-    return new Clone();
+  static get [Symbol.species]() {
+    return Car;
   }
 }
+
+export default EVCar;
